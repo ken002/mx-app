381383951f4bfc47fd8b669ef3d4600e9c8f5e46
jly-git <806784058@qq.com>
2019-11-08 16:50:41 +0800

    88

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,6 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+		
 	</view>
 </template>
 
@@ -11,14 +8,41 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				page:1,
 			}
 		},
 		onLoad() {
-
+			this.selectHotProducts();
+			
+			this.selectProductsOnline();
 		},
 		methods: {
-
+			async selectHotProducts(){
+				const res = await this.$util.request({
+					requestUrl: 'api/products',
+					data: {
+						limit: 10,
+						page: this.page,
+						name: null,
+						hot:1,
+						online:null,
+					}
+				});
+				console.log('广告位：', res);
+			},
+			async selectProductsOnline(){
+				const res = await this.$util.request({
+					requestUrl: 'api/products',
+					data: {
+						limit: 10,
+						page: this.page,
+						name: null,
+						hot:null,
+						online:1,
+					}
+				});
+				console.log('所有上架商品：', res);
+			},
 		}
 	}
 </script>
@@ -29,24 +53,5 @@
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
 	}
 </style>
