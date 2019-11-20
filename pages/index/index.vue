0ab26da9659b8d93fde45c92ac268e6684726952
jly-git <806784058@qq.com>
2019-11-20 16:51:34 +0800

    阿斯弗

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,59 +1,73 @@
 <template>
-	<view class="content">
-		
+	<view>
+		<swiper class="card-swiper" :class="dotStyle?'square-dot':'round-dot'" :indicator-dots="true" :circular="true"
+		 :autoplay="true" interval="5000" duration="500" @change="cardSwiper" indicator-color="#8799a3"
+		 indicator-active-color="#0081ff">
+			<swiper-item v-for="(item,index) in swiperArr" :key="index" :class="cardCur==index?'cur':''">
+				<view class="swiper-item">
+					<image :src="item.image" mode="aspectFill"></image>
+				</view>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				page:1,
-			}
-		},
-		onLoad() {
-			this.selectHotProducts();
+export default {
+	data() {
+		return {
+			page: 1,
+			swiperArr:[],
+			dotStyle:false,
+			cardCur:0,
+		};
+	},
+	onLoad() {
+		this.selectHotProducts();
+
+		this.selectProductsOnline();
+	},
+	methods: {
+		cardSwiper(){
 			
-			this.selectProductsOnline();
 		},
-		methods: {
-			//广告位
-			async selectHotProducts(){
-				const res = await this.$util.request({
-					requestUrl: 'api/products',
-					data: {
-						limit: 10,
-						page: this.page,
-						name: null,
-						hot:1,
-						online:null,
-					}
-				});
-				console.log('广告位：', res);
-			},
-			//上架产品列表
-			async selectProductsOnline(){
-				const res = await this.$util.request({
-					requestUrl: 'api/products',
-					data: {
-						limit: 10,
-						page: this.page,
-						name: null,
-						hot:null,
-						online:1,
-					}
-				});
-				console.log('所有上架商品：', res);
-			},
+		//广告位
+		async selectHotProducts() {
+			const res = await this.$util.request({
+				requestUrl: 'api/products',
+				data: {
+					limit: 10,
+					page: this.page,
+					name: null,
+					hot: 1,
+					online: null
+				}
+			});
+			console.log('广告位：', res);
+			
+			this.swiperArr=res.data.data;
+		},
+		//上架产品列表
+		async selectProductsOnline() {
+			const res = await this.$util.request({
+				requestUrl: 'api/products',
+				data: {
+					limit: 10,
+					page: this.page,
+					name: null,
+					hot: null,
+					online: 1
+				}
+			});
+			console.log('所有上架商品：', res);
 		}
 	}
+};
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+<style lang="scss" scoped>
+	.swiper-image{
+		width: 100%;
+		height: 100%;
 	}
 </style>
